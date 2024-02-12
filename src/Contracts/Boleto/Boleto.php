3410f0fb508557b566f6f600f532fba73849cdac
João Gabriel Xavier <joaogabriel.informaticapgua@gmail.com>
2024-02-12 14:24:28 -0300

    integra com o sisprime
    possibilita a geração de boletos para o banco Sisprime do Brasil
    utilizando o layout de arquivos CNAB 240 para remessa e retorno
    
    foram adicionados os manuais de cobrança para CNAB 240
    e o logotipo do banco para a inclusão no boleto

--- src/Contracts/Boleto/Boleto.php
+++ src/Contracts/Boleto/Boleto.php
@@ -23,6 +23,7 @@ interface Boleto
     const COD_BANCO_DELCRED = '435';
     const COD_BANCO_BV = '655';
     const COD_BANCO_SICREDI = '748';
+    const COD_BANCO_SISPRIME = '084';
     const COD_BANCO_BANRISUL = '041';
     const COD_BANCO_BANCOOB = '756';
     const COD_BANCO_BNB = '004';
